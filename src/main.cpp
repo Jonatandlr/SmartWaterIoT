9fe7291b9a257bed3d72bbf73cc78cbf635a14c2
Jonatandlr <jonatand.larosa@gmail.com>
2024-11-11 17:34:04 -0600

    add DataProcessor integration for averaging sensor data from conductivity and height sensors

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "WifiConnect.h"
 #include "Conductivity.h"
 #include "HeightSensor.h"
+#include "DataProcessor.h"
 
 // Configura tu red WiFi
 const char* ssid = "M16_HUGOS 2728";            
@@ -11,17 +12,38 @@ const char* mqttServer = "192.168.137.76";
 
 WifiConnect wifi(ssid, password, mqttServer, 1883);
 Conductivity conductivity;
-
+HeightSensor heightSensor;
+DataProcessor processorConductivity(10);
+DataProcessor processorHeight(10);
 void setup() {
     Serial.begin(9600);
     wifi.setup_wifi();
     conductivity.begin();
+    heightSensor.begin();
 }
 
 void loop() {
     wifi.begin();
-    float sensorData = conductivity.getConductivity();
-    printf("Conductividad: %f\n", sensorData);
-    // wifi.send_data(sensorData, "esp/conductividad");
+
+    if (processorConductivity.getDataCount() < 10) {
+        float sensorData = conductivity.getConductivity();
+        processorConductivity.addData(sensorData);
+    }
+    if (processorHeight.getDataCount() < 10)
+    {
+        int sensorData = heightSensor.getDistance();
+        processorHeight.addData(sensorData);
+    }
+
+    if (processorConductivity.getDataCount() >= 10 && processorHeight.getDataCount() >= 10){
+        float averageConductivity = processorConductivity.getAverage();
+        int averageHeight = processorHeight.getAverage();
+        Serial.print("El promedio de conductividad es: ");
+        Serial.println(averageConductivity);
+        Serial.print("El promedio de altura es: ");
+        Serial.println(averageHeight);
+        processorConductivity.reset();
+        processorHeight.reset();
+    }
     delay(1000);
 }
